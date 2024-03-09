44c4a7595a76fa268e2ce64ae7623dab8efede6b
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-03-09 12:00:01 +0700

    bla bla

--- classes/player.py
+++ classes/player.py
@@ -1,22 +1,26 @@
 import pygame as py
 
 class Player:
-	def __init__(self, x, y, color, move_left_key, move_right_key, jump_key, side):
+	def __init__(self, x, y, color, move_left_key, move_right_key, jump_key, punch_key, side):
 		self.SQUARE_SIZE_X = 100
 		self.SQUARE_SIZE_Y = 150
 		self.rect = py.Rect(x, y, self.SQUARE_SIZE_X, self.SQUARE_SIZE_Y)
 		self.color = color
 		self.move_left_key = move_left_key
 		self.move_right_key = move_right_key
-		self.jump_key = jump_key
 		self.side = side
 		self.speed = 4
 
 		# use for jump event
+		self.jump_key = jump_key
 		self.on_ground = True;
 		self.square_y_speed = 0
 		self.GRAVITY = 0.5
 		self.JUMP_POWER = -15
+
+		#use for punch
+		self.punch_key = punch_key
+		self.punched = False
 	
 	def move(self, dx, dy):
 		self.rect.move_ip(dx, dy)
@@ -32,6 +36,18 @@ class Player:
 		font = py.font.SysFont(None, 16)
 		text = font.render(' (' + str(self.rect.x) + ',' + str(self.rect.y) + ')', True, (0,0,0))
 		surface.blit(text, ( self.rect.x, 10))
+
+		if self.punched and self.side == 'L':
+			py.draw.rect(surface, (4,166,0), py.Rect(self.rect.x, self.rect.y, 100, 50))
+		elif self.punched:
+			py.draw.rect(surface, (4,166,0), py.Rect(self.rect.x - self.SQUARE_SIZE_X, self.rect.y, 100, 50))
+
+
+	def punch(self, key):
+		if key[self.punch_key]:
+			self.punched = True
+		else :
+			self.punched = False
 
 	def move_logic(self, key):
 		if key[self.move_left_key]:
@@ -53,7 +69,13 @@ class Player:
 			self.on_ground = True
 
 		# Giới hạn không cho khối vuông đi quá biên
-		if self.rect.x < 0:
-			self.rect.x = 0
-		elif self.rect.x > 1200 - self.SQUARE_SIZE_X:
-			self.rect.x = 1200 - self.SQUARE_SIZE_X
+		if self.side == 'R':
+			if self.rect.x < 0:
+				self.rect.x = 0
+			elif self.rect.x > 1200 - self.SQUARE_SIZE_X:
+				self.rect.x = 1200 - self.SQUARE_SIZE_X
+		else :
+			if self.rect.x < self.SQUARE_SIZE_X:
+				self.rect.x = self.SQUARE_SIZE_X
+			elif self.rect.x > 1200 :
+				self.rect.x = 1200
--- main.py
+++ main.py
@@ -8,30 +8,31 @@ py.init()
 screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 py.display.set_caption('Demo')
 
-player1 = Player(300, 350, RED, py.K_a, py.K_d, py.K_SPACE, 'L')
-player2 = Player(800, 350, BLUE, py.K_LEFT, py.K_RIGHT, py.K_RCTRL, 'R')
+player1 = Player(300, 350, RED, py.K_a, py.K_d, py.K_SPACE, py.K_LCTRL, 'L')
+player2 = Player(800, 350, BLUE, py.K_LEFT, py.K_RIGHT, py.K_RCTRL, py.K_KP0, 'R')
 
 run = True
 clock = py.time.Clock()
 while run:
-    screen.fill(WHITE)
-    
-    line_spacing = 50  # Khoảng cách giữa các đường line
-    for y in range(0, SCREEN_HEIGHT, line_spacing):
-        py.draw.line(screen, BLACK, (0, y), (SCREEN_WIDTH, y))
-    line_spacing_vertical = 50
-    for x in range(0, SCREEN_WIDTH, line_spacing_vertical):
-        py.draw.line(screen, BLACK, (x, 0), (x, SCREEN_HEIGHT))
-    
-    for player in [player1, player2]:
-        player.draw(screen)
-        player.move_logic(py.key.get_pressed())
+	screen.fill(WHITE)
+	
+	line_spacing = 50  # Khoảng cách giữa các đường line
+	for y in range(0, SCREEN_HEIGHT, line_spacing):
+		py.draw.line(screen, BLACK, (0, y), (SCREEN_WIDTH, y))
+	line_spacing_vertical = 50
+	for x in range(0, SCREEN_WIDTH, line_spacing_vertical):
+		py.draw.line(screen, BLACK, (x, 0), (x, SCREEN_HEIGHT))
+	
+	for player in [player1, player2]:
+		player.draw(screen)
+		player.move_logic(py.key.get_pressed())
+		player.punch(py.key.get_pressed())
 
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            run = False
+	for event in py.event.get():
+		if event.type == py.QUIT:
+			run = False
 
-    py.display.update()
-    clock.tick(60)
+	py.display.update()
+	clock.tick(60)
 
 py.quit()
