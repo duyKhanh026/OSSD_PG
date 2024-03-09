dec84b6c8161c50d19c8669e43a1e8a88f0a46df
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-03-09 10:08:48 +0700

    bla bla

--- classes/player.py
+++ classes/player.py
@@ -1,17 +1,59 @@
-import pygame
+import pygame as py
 
 class Player:
-	def __init__(self, x, y, color):
-		self.rect = pygame.Rect(x, y, 100, 150)
+	def __init__(self, x, y, color, move_left_key, move_right_key, jump_key, side):
+		self.SQUARE_SIZE_X = 100
+		self.SQUARE_SIZE_Y = 150
+		self.rect = py.Rect(x, y, self.SQUARE_SIZE_X, self.SQUARE_SIZE_Y)
 		self.color = color
+		self.move_left_key = move_left_key
+		self.move_right_key = move_right_key
+		self.jump_key = jump_key
+		self.side = side
+		self.speed = 4
+
+		# use for jump event
+		self.on_ground = True;
+		self.square_y_speed = 0
+		self.GRAVITY = 0.5
+		self.JUMP_POWER = -15
 	
 	def move(self, dx, dy):
 		self.rect.move_ip(dx, dy)
 	
 	def draw(self, surface):
-		pygame.draw.rect(surface, self.color, self.rect)
-		pygame.draw.line(surface, (26,243,0), (self.rect.x, 0), (self.rect.x,600))
-		pygame.draw.line(surface, (26,243,0), (0, self.rect.y), (1200,self.rect.y))
-		font = pygame.font.SysFont(None, 24)
+		if self.side == 'R':
+			py.draw.rect(surface, self.color, self.rect)
+		else :
+			py.draw.rect(surface, self.color, py.Rect(self.rect.x - self.SQUARE_SIZE_X, self.rect.y, self.SQUARE_SIZE_X, self.SQUARE_SIZE_Y))
+
+		py.draw.line(surface, (26,243,0), (self.rect.x, 0), (self.rect.x,600))
+		py.draw.line(surface, (26,243,0), (0, self.rect.y), (1200,self.rect.y))
+		font = py.font.SysFont(None, 16)
 		text = font.render(' (' + str(self.rect.x) + ',' + str(self.rect.y) + ')', True, (0,0,0))
-		surface.blit(text, ( self.rect.x , 10))
+		surface.blit(text, ( self.rect.x, 10))
+
+	def move_logic(self, key):
+		if key[self.move_left_key]:
+			self.move(-self.speed, 0)
+		elif key[self.move_right_key]:
+			self.move(self.speed, 0)
+		if key[self.jump_key] and self.on_ground:
+			self.square_y_speed = self.JUMP_POWER
+			self.on_ground = False
+
+		# Áp dụng trọng lực
+		self.square_y_speed += self.GRAVITY
+		self.rect.y += self.square_y_speed
+		
+		# Kiểm tra va chạm với mặt đất
+		if self.rect.y>= 600 - self.SQUARE_SIZE_Y:
+			self.rect.y= 600 - self.SQUARE_SIZE_Y
+			self.square_y_speed = 0
+			self.on_ground = True
+
+		# Giới hạn không cho khối vuông đi quá biên
+		if self.rect.x < 0:
+			self.rect.x = 0
+		elif self.rect.x > 1200 - self.SQUARE_SIZE_X:
+			self.rect.x = 1200 - self.SQUARE_SIZE_X
--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-import pygame
-from classes.player import Player  # Import lớp Player từ tệp player.py
+import pygame as py
+from classes.player import Player  
 from values.color import *
 from values.screen import *
 
-pygame.init()
+py.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Demo')
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+py.display.set_caption('Demo')
 
-player1 = Player(300, 350, RED)
-player2 = Player(800, 350, BLUE)
+player1 = Player(300, 350, RED, py.K_a, py.K_d, py.K_SPACE, 'L')
+player2 = Player(800, 350, BLUE, py.K_LEFT, py.K_RIGHT, py.K_RCTRL, 'R')
 
 run = True
-clock = pygame.time.Clock()
+clock = py.time.Clock()
 while run:
-	
-	screen.fill(WHITE)
-	line_spacing = 50  # Khoảng cách giữa các đường line
-	for y in range(0, SCREEN_HEIGHT, line_spacing):
-		pygame.draw.line(screen, BLACK, (0, y), (SCREEN_WIDTH, y))
-	line_spacing_vertical = 50
-	for x in range(0, SCREEN_WIDTH, line_spacing_vertical):
-	    pygame.draw.line(screen, BLACK, (x, 0), (x, SCREEN_HEIGHT))
-	
-	for player in [player1, player2]:
-		player.draw(screen)
+    screen.fill(WHITE)
+    
+    line_spacing = 50  # Khoảng cách giữa các đường line
+    for y in range(0, SCREEN_HEIGHT, line_spacing):
+        py.draw.line(screen, BLACK, (0, y), (SCREEN_WIDTH, y))
+    line_spacing_vertical = 50
+    for x in range(0, SCREEN_WIDTH, line_spacing_vertical):
+        py.draw.line(screen, BLACK, (x, 0), (x, SCREEN_HEIGHT))
+    
+    for player in [player1, player2]:
+        player.draw(screen)
+        player.move_logic(py.key.get_pressed())
 
-	key = pygame.key.get_pressed()
-	if key[pygame.K_a]:
-		player1.move(-2, 0)
-	elif key[pygame.K_d]:
-		player1.move(2, 0)
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            run = False
 
-	if key[pygame.K_LEFT]:
-		player2.move(-2, 0)
-	elif key[pygame.K_RIGHT]:
-		player2.move(2, 0)
+    py.display.update()
+    clock.tick(60)
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-
-	pygame.display.update()
-	clock.tick(60)
-
-pygame.quit()
+py.quit()
